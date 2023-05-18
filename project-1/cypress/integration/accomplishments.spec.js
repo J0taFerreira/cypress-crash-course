6b8812225d18dd70751581291bc4f61d9ade81da
J0taFerreira <joao.ferreira@encora.comgit config --global user.name "Joo Paulo"git config --global user.email joao.ferreira@encora.com>
2023-05-18 16:03:09 -0300

    project 1 done

--- project-1/cypress/integration/accomplishments.spec.js
+++ project-1/cypress/integration/accomplishments.spec.js
@@ -19,4 +19,15 @@ describe("Accomplishments Dashboard", () =>{
         cy.contains("Submit Accomplishment").click()
         cy.contains("This Accomplisment was Successfully Submitted").should("be.visible")
     })
+
+        it("Should go back to accomplishment with empty input", () => {
+        cy.get("[data-cy='accomplishment-title-input']").type("My Basketball Accomplishment")
+        cy.get("[data-cy='accomplishment-input']").type("I made 10 threes in a row")
+        cy.get("[type='checkbox']").click()
+        cy.contains("Submit Accomplishment").click()
+        cy.contains("Go Back").click()
+        cy.get("[data-cy='accomplishment-title-input']").should("have.value", "")
+        cy.get("[data-cy='accomplishment-input']").should("have.value", "")
+        cy.get("[type='checkbox'").should("not.be.checked")
+    })
 })
